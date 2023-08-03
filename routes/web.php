e39a66178d9be5e7023080d866507eda721e4f69
Iqbal Pamula <balpamula@gmail.com>
2023-08-03 07:49:04 +0700

    episode 4

--- routes/web.php
+++ routes/web.php
@@ -34,11 +34,13 @@ Route::get('/blog', function () {
     $blog_posts = [
         [
             "title" => "Post Pertama",
+            "slug" => "post-pertama",
             "author" => "Iqbal",
             "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, quo. Odit, maiores non! In temporibus deserunt, rerum quo vitae maxime."
         ],
         [
             "title" => "Post Kedua",
+            "slug" => "post-kedua",
             "author" => "Pamula",
             "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, tempore hic odio rerum corporis obcaecati illo iusto. Rerum inventore nisi magnam aspernatur a quo, minima sed rem eveniet corporis eligendi similique nobis eos maxime quasi assumenda eaque explicabo non ipsa ab qui nemo. Reprehenderit officiis rem fugiat libero sit quae."
         ]
@@ -46,5 +48,37 @@ Route::get('/blog', function () {
     return view('posts', [
         "title" => "Posts",
         "posts" => $blog_posts
+    ]);
+});
+
+// halaman single posts
+
+Route::get('/posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "title" => "Post Pertama",
+            "slug" => "post-pertama",
+            "author" => "Iqbal",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, quo. Odit, maiores non! In temporibus deserunt, rerum quo vitae maxime."
+        ],
+        [
+            "title" => "Post Kedua",
+            "slug" => "post-kedua",
+            "author" => "Pamula",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, tempore hic odio rerum corporis obcaecati illo iusto. Rerum inventore nisi magnam aspernatur a quo, minima sed rem eveniet corporis eligendi similique nobis eos maxime quasi assumenda eaque explicabo non ipsa ab qui nemo. Reprehenderit officiis rem fugiat libero sit quae."
+        ]
+    ];
+
+    $new_post = [];
+
+    foreach ($blog_posts as $post) {
+        if ($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
     ]);
 });
