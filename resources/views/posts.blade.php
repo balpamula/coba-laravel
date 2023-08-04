cf032c9e1ad9a4daf4a2f7ad52cb14613421c9c2
Iqbal Pamula <balpamula@gmail.com>
2023-08-04 18:59:42 +0700

    episode 11

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1 class="mb-4">Halaman Blog Posts</h1>
+    <h1 class="mb-4">{{ $title }}</h1>
 
     @foreach ($posts as $post)
         <article class="mb-4 border-bottom pb-4">
