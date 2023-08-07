39c37526a42d95e445d23252f8f367c3212de679
Iqbal Pamula <balpamula@gmail.com>
2023-08-07 20:07:29 +0700

    episode 16

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -15,18 +15,34 @@
             {{ session('success') }}
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
+        @elseif(session()->has('loginError'))
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            {{ session('loginError') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
         @endif
 
         <main class="form-signin w-100 m-auto">
             <h1 class="h3 mb-3 fw-normal text-center">Masuk dulu</h1>
-            <form>        
+            <form action="/login" method="POST">
+                @csrf      
                 <div class="form-floating">
-                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                    <label for="floatingInput">Email address</label>
+                    <input type="username" name="username" class="form-control rounded-0 rounded-top @error('username') is-invalid @enderror" id="username" placeholder="Username" autofocus required value="{{ old('username') }}">
+                    <label for="floatingInput">Username</label>
+                    @error('username')
+                    <div class="invalid-feedback mb-1">
+                        {{ $message }}
+                    </div>
+                    @enderror
                 </div>
                 <div class="form-floating">
-                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                    <input type="password" name="password" class="form-control rounded-0 rounded-bottom" id="password" placeholder="Password">
                     <label for="floatingPassword">Password</label>
+                    @error('password')
+                    <div class="invalid-feedback mb-1">
+                        {{ $message }}
+                    </div>
+                    @enderror
                 </div>
                 <button class="btn btn-primary w-100 py-2 mt-3" type="submit">Masuk</button>
             </form>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
@@ -48,7 +49,11 @@ Route::get('/categories', function () {
     ]);
 });
 
-Route::get('/login', [LoginController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index'])->middleware('guest')->name('login');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
 
 Route::get('/register', [RegisterController::class, 'index']);
 Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
