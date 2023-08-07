c4c60de121955f0ff7b04ffd77390a5482cacc07
Iqbal Pamula <balpamula@gmail.com>
2023-08-07 16:41:14 +0700

    episode 15

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -12,22 +12,43 @@
 
         <main class="form-registration">
             <h1 class="h3 mb-3 fw-normal text-center">Form Daftar</h1>
-            <form>        
+            <form action="/register" method="POST">
+                @csrf
                 <div class="form-floating">
-                    <input type="name" class="form-control rounded-0 rounded-top" id="floatingInput" placeholder="Nama">
+                    <input type="text" name="name" class="form-control rounded-0 rounded-top @error('name') is-invalid @enderror" id="floatingInput" placeholder="Nama" required value="{{ old('name') }}">
                     <label for="floatingInput">Name</label>
+                    @error('name')
+                    <div class="invalid-feedback mb-1">
+                        {{ $message }}
+                    </div>
+                    @enderror
                 </div>
                 <div class="form-floating">
-                    <input type="username" class="form-control rounded-0" id="floatingPassword" placeholder="Username">
+                    <input type="text" name="username" class="form-control rounded-0 @error('username') is-invalid @enderror" id="floatingPassword" placeholder="Username" required value="{{ old('username') }}">
                     <label for="floatingPassword">Username</label>
+                    @error('username')
+                    <div class="invalid-feedback mb-1">
+                        {{ $message }}
+                    </div>
+                    @enderror
                 </div>
                 <div class="form-floating">
-                    <input type="email" class="form-control rounded-0" id="floatingPassword" placeholder="name@example.com">
+                    <input type="email" name="email" class="form-control rounded-0 @error('email') is-invalid @enderror" id="floatingPassword" placeholder="name@example.com" required value="{{ old('email') }}">
                     <label for="floatingPassword">Email</label>
+                    @error('email')
+                    <div class="invalid-feedback mb-1">
+                        {{ $message }}
+                    </div>
+                    @enderror
                 </div>
                 <div class="form-floating">
-                    <input type="password" class="form-control rounded-0 rounded-bottom" id="floatingPassword" placeholder="Password">
+                    <input type="password" name="password" class="form-control rounded-0 rounded-bottom @error('password') is-invalid @enderror" id="floatingPassword" placeholder="Password" required>
                     <label for="floatingPassword">Password</label>
+                    @error('password')
+                    <div class="invalid-feedback mb-1">
+                        {{ $message }}
+                    </div>
+                    @enderror
                 </div>
                 <button class="btn btn-primary w-100 py-2 mt-3" type="submit">Daftar</button>
             </form>
