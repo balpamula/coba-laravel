6a9d8065db99cc40054a4ad705ee7a357b454c3f
Iqbal Pamula <balpamula@gmail.com>
2023-08-11 00:48:47 +0700

    episode 23

--- resources/views/dashboard/categories/index.blade.php
+++ resources/views/dashboard/categories/index.blade.php
@@ -2,37 +2,35 @@
 
 @section('container')
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Posts oleh : {{ auth()->user()->name }}</h1>
+        <h1 class="h2">Posts Categories</h1>
     </div>
 
     @if(session()->has('success'))
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
+    <div class="alert alert-success alert-dismissible fade show col-lg-6" role="alert">
         {{ session('success') }}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
     @endif
 
-    <div class="table-responsive">
-        <a href="/dashboard/posts/create" class="btn btn-primary"><span data-feather="plus-circle"></span> Tambah Posts</a>
+    <div class="table-responsive col-lg-6">
+        <a href="/dashboard/categories/create" class="btn btn-primary"><span data-feather="plus-circle"></span> Tambah Posts</a>
             <table class="table table-striped table-sm my-3">
                 <thead>
                     <tr>
                         <th scope="col">#</th>
-                        <th scope="col">Title</th>
-                        <th scope="col">Category</th>
+                        <th scope="col">Name</th>
                         <th scope="col">Action</th>
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($posts as $post)
+                    @foreach ($categories as $category)
                     <tr>
                         <td>{{ $loop->iteration }}</td>
-                        <td>{{ $post->title }}</td>
-                        <td>{{ $post->category->name }}</td>
+                        <td>{{ $category->name }}</td>
                         <td>
-                            <a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info"><span data-feather="eye"></span></a>
-                            <a href="/dashboard/posts/{{ $post->slug }}/edit" class="badge bg-warning"><span data-feather="edit"></span></a>
-                            <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+                            <a href="/dashboard/categories/{{ $category->slug }}" class="badge bg-info"><span data-feather="eye"></span></a>
+                            <a href="/dashboard/categories/{{ $category->slug }}/edit" class="badge bg-warning"><span data-feather="edit"></span></a>
+                            <form action="/dashboard/categories/{{ $category->slug }}" method="post" class="d-inline">
                             @method('delete')
                             @csrf 
                                 <button class="badge bg-danger border-0" onclick="return confirm('Apakah anda yakin untuk menghapus data?')"><span data-feather="trash-2"></span></button>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminCategoryController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DashPostController;
 use App\Models\Category;
@@ -63,3 +64,5 @@ Route::get('/dashboard', function () {
 
 Route::get('/dashboard/posts/checkSlug', [DashPostController::class, 'checkSlug']);
 Route::resource('/dashboard/posts', DashPostController::class)->middleware('auth');
+
+Route::resource('/dashboard/categories', AdminCategoryController::class)->middleware('admin');
